eabe6d95886ec36bf25762dcfcddca4647b19f4b
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-05-28 00:01:49 +0300

    Added closed by Escape key

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { Overlay, ModalImg } from './Modal.styled';
 
 export class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', event => {
+      if (event.code === 'Escape') {
+        this.props.closeModal();
+      }
+    });
+  }
+
   render() {
     return (
       <Overlay onClick={this.props.closeModal}>
