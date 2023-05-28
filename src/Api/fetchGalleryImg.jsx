205a925cadc99d87976d9f597abc18bc87e5c2e7
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-05-28 16:14:26 +0300

    Added new Error

--- src/Api/fetchGalleryImg.jsx
+++ src/Api/fetchGalleryImg.jsx
@@ -5,6 +5,10 @@ const PerPage = 12;
 export function fetchGalleryImg(searchQuery, page) {
   return fetch(
     `${URL}?q=${searchQuery}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${PerPage}&page=${page}`
-  ).then(response => response.json());
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
-
